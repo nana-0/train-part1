4937b11a6746943bb671ae7b55a6de5a9c5cddd6
nana-0 <2635591841@qq.com>
2020-10-25 11:21:20 +0800

    meituan

--- meituan/src/App.vue
+++ meituan/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    meituan
+    <router-view></router-view>
   </div>
 </template>
 
--- meituan/src/router/index.js
+++ meituan/src/router/index.js
@@ -1,9 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Index from '@/views/home/index'
+import Detail from '@/views/detail/index'
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path:'/',
+    component:Index
+  },
+  {
+    path:'/detail',
+    component:Detail
+  }
   
 ]
 
