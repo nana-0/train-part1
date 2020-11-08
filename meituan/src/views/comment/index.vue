2fef3f51d16ff33790ec5a2f97d3718891823d5a
nana-0 <2635591841@qq.com>
2020-11-08 09:08:04 +0800

    meituan

--- meituan/src/views/comment/index.vue
+++ meituan/src/views/comment/index.vue
@@ -1,14 +1,31 @@
 <template>
     <div>
-        <comment-header></comment-header>
+        <comment-header :list="list"></comment-header>
+        <comment-list :list="list"></comment-list>
     </div>
 </template>
 
 <script>
-import CommentHeader from "./CommentHeader"
+import axios from "axios";
+import CommentHeader from "./CommentHeader";
+import CommentList from "./CommentList";
     export default {
+        data(){
+            return{
+                id:this.$route.query.id,
+                list:{}
+            }
+        },
         components:{
-            CommentHeader
+            CommentHeader,
+            CommentList
+        },
+        created(){
+            axios.get(`http://admin.gxxmglzx.com/tender/test/get_info?id=${this.id}&type=1`)
+            .then((res)=>{
+                console.log(res.data.data)
+                this.list = res.data.data
+            })
         }
     }
 </script>
