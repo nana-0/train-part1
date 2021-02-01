5cd288bcb1678e3b937c3b86a0d5ad6ea192e65a
nana-0 <2635591841@qq.com>
2021-02-01 20:13:38 +0800

    字符串对象转换

--- node/day1/05-buffer2.js
+++ node/day1/05-buffer2.js
@@ -14,14 +14,21 @@
 // }
 // console.log(buf1.toString('ascii'));
 
-const buf2 = Buffer.allocUnsafeSlow(26);
-const buf3 = Buffer.allocUnsafeSlow(26).fill("!");
-console.log(buf2);
-console.log(buf3.toString("ascii"));
+// const buf2 = Buffer.allocUnsafeSlow(26);
+// const buf3 = Buffer.allocUnsafeSlow(26).fill("!");
+// console.log(buf2);
+// console.log(buf3.toString("ascii"));
 
-for(let i = 0;i<26;i++){
-    buf2[i] = i+97;
-}
-console.log(buf2);
-buf2.copy(buf3,8,16,20);//拷贝buf2从第16到19字节偏移量的数据buf3第8字节偏移量
-console.log(buf3.toString('ascii',0,25));
+// for(let i = 0;i<26;i++){
+//     buf2[i] = i+97;
+// }
+// console.log(buf2);
+// buf2.copy(buf3,8,16,20);//拷贝buf2从第16到19字节偏移量的数据buf3第8字节偏移量
+// console.log(buf3.toString('ascii',0,25));
+
+
+const buf5 = Buffer.from("hello");
+const buf6 = Buffer.from("world");
+
+console.log(Buffer.compare(buf5,buf6));
+console.log(buf5.equals(buf6));
