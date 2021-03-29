8d3e480f9d3c43c710ea96fbf1fbd32ad1122eed
nana-0 <2635591841@qq.com>
2021-03-29 11:37:14 +0800

    my-app

--- nana/React/create/my-app/src/App.js
+++ nana/React/create/my-app/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link, Route } from 'react-router-dom'
+import Home from './view/Home'
+import Detail from './view/Detail'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      test
+      <Link to='/home'>首页</Link>
+      <Link to='/detail'>详情页</Link>
+
+      <Route path='/' exact component={Home}></Route>
+
+      {/* <Route path='/home' component={Home}></Route> */}
+      <Route path='/detail' component={Detail}></Route>
+
     </div>
   );
 }
